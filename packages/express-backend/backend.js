bf28bdf6d2a590c9352e1f31c09fbac5f6c650c3
Names3tma <ethancma@gmail.com>
2026-09-09 13:14:19 -0700

    IE2: Final Submission

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -6,6 +6,10 @@ const port = 8000;
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
@@ -19,15 +23,64 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
+const findUserByNameAndJob = (name, job) => {
+  return users["users_list"].filter((user) => user["name"] === name && user["job"] === job);
+};
+
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  const job = req.query.job
+  if (name != undefined && job != undefined) {
+    let result = findUserByNameAndJob(name, job);
+    result = { users_list: result };
+    res.send(result);
+  } 
+  if(name != undefined)
+  {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  }
+  else {
+    res.send(users);
+  } 
+});
+
 const addUser = (user) => {
   users["users_list"].push(user);
   return user;
 };
 
 app.post("/users", (req, res) => {
-  const userToAdd = req.body;
+  const userToAdd = req.body; 
   addUser(userToAdd);
   res.send();
+});
+
+const deleteUser = (user) => {
+    for(let i = 0; i < users["users_list"].length; i++)
+    {
+        if(users["users_list"][i].id === user.id)
+        {
+            for(let j = i; j < users["users_list"].length-1; j++)
+            {
+                users["users_list"][j] = users["users_list"][j+1]
+            }
+            users["users_list"].length = users["users_list"].length - 1
+            return user;
+        }
+    }
+    return null;
+}
+
+app.delete("/users", (req, res) => {
+    const userToDelete = req.body;
+    deleteUser(userToDelete);
+    res.send()
 });
 
 app.listen(port, () => {
@@ -58,6 +111,11 @@ const users = {
     },
     {
       id: "zap555",
+      name: "Dennis",
+      job: "Bartender",
+    },
+    {
+      id: "den676",
       name: "Dennis",
       job: "Bartender",
     },
